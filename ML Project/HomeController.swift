fa1f62ba207c70f2d884c30a89c5fad4dab22d7d
Ugo Dos Reis <udosrei9@3012-l29527-s.local>
2019-05-09 12:36:01 -0500

    Trying out shadows

--- ML Project/HomeController.swift
+++ ML Project/HomeController.swift
@@ -16,11 +16,36 @@ import UIKit
 
 class HomeController: UIViewController {
 
+    @IBOutlet weak var uploadButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        uploadButton.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        uploadButton.layer.cornerRadius = uploadButton.frame.size.height/2
+        uploadButton.layer.applySketchShadow(color: #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), alpha: 0.5, x: 0, y: 0, blur: 10, spread: 10)
+        
     }
 
 
 }
-
+extension CALayer {
+    func applySketchShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4,
+        spread: CGFloat = 0)
+    {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+}
