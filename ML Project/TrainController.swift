93e87b2a16dfe69b7ded3d0cee21044ec438a861
Anupam Murikurthy 2019 <amuriku9@3012-l29546-s.local>
2019-05-13 12:42:07 -0500

    Added image picker and upload button

--- ML Project/TrainController.swift
+++ ML Project/TrainController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class TrainController: UIViewController {
-
+class TrainController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+let imagePicker = UIImagePickerController()
+    
+var faceImage: UIImage!
+    var faceImageArray: [UIImage] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
     
@@ -26,5 +29,36 @@ class TrainController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("Picked image")
+        
+           self.faceImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        self.faceImageArray.append(self.faceImage)
+        print(self.faceImageArray.count)
+        imagePicker.dismiss(animated: true){
+            
+            
+           
+         
+            
 
+            
+            
+        }
+        
+    }
+    
+    
+
+    @IBAction func uploadButtonTapped(_ sender: Any) {
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+        
+
+    }
+    @IBAction func trainButtonTapped(_ sender: Any) {
+        print("Send photos to server")
+    }
 }
