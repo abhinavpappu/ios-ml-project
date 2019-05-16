e1e23417b082e5984c6515561fea9f32dd2c9b68
Ugo Dos Reis <udosrei9@3012-l29527-s.local>
2019-05-16 12:06:10 -0500

    Adding Table - Ugo

--- ML Project/HistoryController.swift
+++ ML Project/HistoryController.swift
@@ -8,23 +8,19 @@
 
 import UIKit
 
-class HistoryController: UIViewController {
+class HistoryController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var history: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return history.count
     }
-    */
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellId", for: indexPath)
+        cell.textLabel?.text = String(history[indexPath.item])
+        return cell
+    }
 }
